f0bf87a5e9bdf9f7cbc9a2a982b85dd5969bac03
Nofo <96763105+sainofo@users.noreply.github.com>
2023-10-29 23:53:29 -0700

    main.cpp

---
+++ MA1 /main.cpp
@@ -0,0 +1,17 @@
+/ This code was taken from https://www.techiedelight.com/queue-implementation-cpp/
+// The code has been modified from the original to provide opportunities to learn
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+// define default capacity of the queue
+#define SIZE 10
+
+// main function
+int main()
+{
+    // call your test functions here!
+
+    return 0;
+}
