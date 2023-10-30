f4c6296773afea4ded0f65aa7142ba60e6ac80d4
Nofo <96763105+sainofo@users.noreply.github.com>
2023-10-30 16:49:45 -0700

    header.hpp

---
+++ PA1/header.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::fstream;
+using std::ifstream;
+using std::ofstream;
+using std::ostream;
