ed4c5809487711350daff8a2539ab9eb5adf2e4a
Nofo <96763105+sainofo@users.noreply.github.com>
2023-10-30 16:53:34 -0700

    BST.h

---
+++ MA2/BST.h
@@ -0,0 +1,239 @@
+#ifndef __BST_H
+#define __BST_H
+
+#include <iostream>
+#include <limits>
+#include <queue>
+using namespace std;
+
+
+/*
+ *  Data structure for a single tree node
+ */
+template <typename T>
+struct Node {
+public:
+	T value;
+	Node* left;
+	Node* right;
+
+	Node(T val) {
+		this->value = val;
+		this->left = nullptr;
+		this->right = nullptr;
+	}
+
+	~Node()
+	{
+		this->value = 0;
+		this->left = nullptr;
+		this->right = nullptr;
+	}
+};
+
+/*
+ * Binary Search Tree (BST) class implementation
+ */
+template <typename T>
+class BST {
+
+protected:
+	Node<T>* _root;         // Root of the tree nodes
+
+	/* Add new T val to the tree */
+	void addHelper(Node<T>* root, T val) {
+		if (root->value > val) {
+			if (!root->left) {
+				root->left = new Node<T>(val);
+			}
+			else {
+				addHelper(root->left, val);
+			}
+		}
+		else {
+			if (!root->right) {
+				root->right = new Node<T>(val);
+			}
+			else {
+				addHelper(root->right, val);
+			}
+		}
+	}
+
+	/* Print tree out in inorder (A + B) */
+	void printInOrderHelper(Node<T>* root) {
+		if (!root) return;
+		printInOrderHelper(root->left);
+		cout << root->value << ' ';
+		printInOrderHelper(root->right);
+	}
+
+	/* Return number of nodes in tree */
+	int nodesCountHelper(Node<T>* root) {
+		if (!root) {
+			return 0;
+		}
+		else {
+			return 1 + nodesCountHelper(root->left) + nodesCountHelper(root->right);
+		}
+	}
+
+	/* Return height of tree (root == nullptr -> 0) */
+	int heightHelper(Node<T>* root) {
+		if (!root) {
+			return -1;
+		}
+		else {
+			return 1 + max(heightHelper(root->left), heightHelper(root->right));
+		}
+	}
+
+	/* Delete a given <T> value from tree */
+	bool deleteValueHelper(Node<T>* parent, Node<T>* current, T value) {
+		if (!current) return false;
+		if (current->value == value) {
+			if (current->left == nullptr || current->right == nullptr) {
+				Node<T>* temp = current->left;
+				if (current->right) temp = current->right;
+				if (parent) {
+					if (parent->left == current) {
+						parent->left = temp;
+					}
+					else {
+						parent->right = temp;
+					}
+				}
+				else {
+					this->_root = temp;
+				}
+			}
+			else {
+				Node<T>* validSubs = current->right;
+				while (validSubs->left) {
+					validSubs = validSubs->left;
+				}
+				T temp = current->value;
+				current->value = validSubs->value;
+				validSubs->value = temp;
+				return deleteValueHelper(current, current->right, temp);
+			}
+			delete current;
+			return true;
+		}
+		return deleteValueHelper(current, current->left, value) ||
+			deleteValueHelper(current, current->right, value);
+	}
+
+	/********************************* PUBLIC API *****************************/
+public:
+
+	BST() : _root(nullptr) { 
+		_root->left = NULL;
+		_root->right = NULL;
+		_root->value = NULL;
+	}               // Basic initialization constructor
+
+	/**
+	 * Destructor - Needs to free *all* nodes in the tree
+	 * TODO: Implement Destructor
+	 */
+
+	~BST() {
+	    cout << "TODO: Implement Destructor" << endl;
+
+		delete left;
+		delete right;
+		delete value;
+	}
+
+	/* Public API */
+	void add(T val) {
+		if (this->_root) {
+			this->addHelper(this->_root, val);
+		}
+		else {
+			this->_root = new Node<T>(val);
+		}
+	}
+
+	void print() {
+		printInOrderHelper(this->_root);
+	}
+
+	/**
+	 * Print the nodes level by level, starting from the root
+	 * TODO: Implement printLevelOrder
+	 */
+	void printLevelOrder() {
+		cout << "TODO: Implement printLevelOrder" << endl; 
+
+		Node<T> *root;
+		ostream &out;
+
+		if(!root){
+			return;
+		}
+		
+		queue <Node<T> *> q;
+		q.push(root);
+		while(q.size() > 0){
+			int queueSize = q.size;
+
+			while(queueSize > 0){
+				Node<T> * node  q.front();
+				q.pop();
+				if(node->left ! = nullptr && node->right != nullptr){
+					q.push(node->left);
+					q.push(node->right);
+				}
+				return q;
+			}
+			cout << node->value << "" <<endl;
+			queueSize--;
+		}
+	}
+
+	int nodesCount() {
+		return nodesCountHelper(this->_root);
+	}
+
+	int height() {
+		return heightHelper(this->_root);
+	}
+
+	/**
+	 * Print the maximum path in this tree
+	 * TODO: Implement printMaxPath
+	 */
+	void printMaxPath() {
+		cout << "TODO: Implement printMaxPath" << endl;
+		Node<T> *root;
+
+		while(!root){
+			cout << root->value <<' '<endl;
+			if(heightHelper(root->left) > heightHelper(root->right)){
+				printMaxPath(root->left);
+			}
+			else{
+				printMaxPath(root->right);
+			}
+		}
+	}
+
+	bool deleteValue(T value) {
+		return this->deleteValueHelper(nullptr, this->_root, value);
+	}
+
+	/**
+	 * Find if the BST contains the value
+	 * TODO: Implement contains
+	 */
+	bool contains(T value) {
+	    cout << "TODO: Implement contains" << endl;
+		if(this->root = value){
+		return numeric_limits<T>::min();
+		}
+	}
+};
+
+#endif
