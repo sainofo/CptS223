5551618494322b5b264fff0ca5dd4df6a9015b98
Nofo <96763105+sainofo@users.noreply.github.com>
2023-10-30 16:48:10 -0700

    main.cpp

---
+++ PA1/main.cpp
@@ -0,0 +1,21 @@
+/*
+* Nofo Fanene
+* CptS 223 PA 1
+* 9.17.23
+*/
+
+#include "Header.hpp"
+#include "LinkedList.hpp"
+#include "Game.hpp"
+
+int main()
+{
+	linked_list a;
+	a.add_node(1);
+	a.add_node(2);
+
+	Game g;
+	g.GameMenu();
+	
+	return 0;
+}
