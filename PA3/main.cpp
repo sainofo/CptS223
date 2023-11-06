55948030ecd7b7fc31f8b6e70078977943d389a9
Nofo <96763105+sainofo@users.noreply.github.com>
2023-11-05 18:43:45 -0800

    main.cpp

---
+++ PA3/main.cpp
@@ -0,0 +1,75 @@
+#include <map>
+#include <string>
+#include <chrono>
+#include <cassert>
+#include "ChainingHash.h"
+#include "ProbingHash.h"
+#include "Hash.h"
+#include <iostream>
+#include <fstream>
+
+void testHash(Hash<int, int> *hash, std::ofstream &myfile)
+{
+
+    myfile << "current size: " << hash->size() << " bucket count: " << hash->bucket_count()
+           << " load factor: " << hash->load_factor() << std::endl;
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    for (int i = 0; i < 100000; ++i)
+    {
+        assert(hash->insert(std::make_pair(i, i)));
+    }
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    myfile << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[ms]" << std::endl;
+
+    begin = std::chrono::steady_clock::now();
+    assert((*hash)[97] == 97);
+    end = std::chrono::steady_clock::now();
+    myfile << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[ms]" << std::endl;
+
+    begin = std::chrono::steady_clock::now();
+    hash->erase(97);
+    end = std::chrono::steady_clock::now();
+    myfile << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[ms]" << std::endl;
+
+    begin = std::chrono::steady_clock::now();
+    assert((*hash)[10000] == 10000);
+    end = std::chrono::steady_clock::now();
+    myfile << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[ms]" << std::endl;
+
+    myfile << "current size: " << hash->size() << " bucket count: " << hash->bucket_count()
+           << " load factor: " << hash->load_factor() << std::endl;
+    hash->clear();
+}
+
+int main(int argc, char *argv[])
+{
+    std::ofstream myfile;
+    myfile.open("output.txt");
+   // ChainingHash<int, int> cHash(101);
+   ProbingHash<int, int> pHash(101);
+
+    for(int i = 0; i < 100000; i++){
+        pHash = i * 2;
+    }
+
+    int num97 = pHash[97];
+    if(num97 == -1){
+        cout << "97 is not found." << endl;
+    }
+    else{
+        cout << "value for 97: " << num97 << endl;
+    }
+
+    pHash[97] == -1;
+
+    int num10k = pHash[10000];
+    if(num10k == -1){
+        cout << "10000 is not found." << endl;
+    }
+    else{
+        cout << "value for 10000: " << num10k << endl;
+    }
+    // testHash(&cHash, myfile);
+    testHash(&pHash, myfile);
+    myfile.close();
+}
